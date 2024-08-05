64c9d9f4dd4b9d2b61c73f71a6bd03f00188d58b
MYJCONAN-work-DELL <273987074@qq.com>
2024-08-05 15:45:59 +0800

    修改仓库地址

--- k8s/docker.sh
+++ k8s/docker.sh
@@ -1,3 +1,3 @@
-docker login 172.18.1.157
-docker build -t 172.18.1.157/public/dahantc-exporter:v1 D:/git_repository/python/DailyWork/大汉三通/dahantc-exporter/k8s
-docker push 172.18.1.157/public/dahantc-exporter:v1
+docker login myharbor.com
+docker build -t myharbor.com/public/dahantc-exporter:v1 D:/git_repository/myself/dahantc-exporter/k8s
+docker push myharbor.com/public/dahantc-exporter:v1
